0b637a0c99de04060230d65d278f3c45d5e81294
makosbikaboka <zoltan.nagy0314@gmail.com>
2023-07-27 20:09:30 +0200

    add indey style app

--- age-calculator-app-main/app.js
+++ age-calculator-app-main/app.js
@@ -15,49 +15,62 @@ function age() {
   // Change .input-block-title font color
   const collection = document.getElementsByClassName("input-block-title");
 
+  const date = new Date();
+  
   // actual date
-  let date = new Date ();
   let y2 = date.getFullYear();
-  let m2 = 1 + date.getMonth();
+  let m2 = date.getMonth() + 1;
   let d2 = date.getDate();
 
-  let month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  // day of the months
+  const month = (y, m) => new Date(y, m, 0).getDate();
 
-  // calculate the birth-date
-  if (d1 > d2){
-      d2 = d2 + month[m2 - 1];
-      m2 = m2 -1;
+  // calc birth day
+  let d = d2 - d1;
+  if (d < 1) {
+    m2--;
+    d = month(y2, m2) - (-d);
   }
 
-  if (m1 > m2){
-      m2 = m2 + 12;
-      y2 = y2 - 1;
+  let m = m2 - m1;
+  if (m < 1) {
+    y2--;
+    m = 12 - (-m);
   }
+
+  let y = y2 - y1;
 
   // error-test segment
   let messageD = document.getElementById("error_message_d");
   let messageM = document.getElementById("error_message_m");
   let messageY = document.getElementById("error_message_y");
+  let month2 = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   messageD.innerHTML = "";
   messageM.innerHTML = "";
   messageY.innerHTML = "";
 
-  if(d1 > month[m1-1]){
+  if(d1 > month2){
     messageD.innerHTML = "Must be a valid day";
     dayInput.classList.add('error');
     collection[0].style.color = 'hsl(0, 100%, 67%)';
+  }else{
+    collection[0].style.color = 'hsl(0, 0%, 8%)';
   }
   
-  if(m1 > m2){
+  if(m1 > 12){
     messageM.innerHTML = "Must be a valid month";
     monthInput.classList.add('error');
     collection[1].style.color = 'hsl(0, 100%, 67%)';
+  }else{
+    collection[1].style.color = 'hsl(0, 0%, 8%)';
   }
 
   if(y1 > y2){
     messageY.innerHTML = "Must be in the past";
     yearInput.classList.add('error');
     collection[2].style.color = 'hsl(0, 100%, 67%)';
+  }else{
+    collection[2].style.color = 'hsl(0, 0%, 8%)';
   }
 
   //if null
@@ -82,11 +95,7 @@ function age() {
     return false;
   }
 
-  // write the result
-  let d = d2 - d1;
-  let m = m2 - m1;
-  let y = y2 - y1;
-
+  //write the result
   document.getElementById('calc-years').innerHTML = y;
   document.getElementById('calc-months').innerHTML = m;
   document.getElementById('calc-days').innerHTML = d;
